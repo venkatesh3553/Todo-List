1becf36c308a4b3108f6ba75f25168b4306fa895
venkatesh3553 <pvenky985@gmail.com>
2024-10-11 16:18:36 +0000

    why text case fail

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,43 +1,57 @@
 import {Component} from 'react'
+import {v4 as uuid} from 'uuid'
 import TodoItem from '../TodoItem'
 import './index.css'
+
 const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    isTrue: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    isTrue: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    isTrue: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    isTrue: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    isTrue: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    isTrue: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    isTrue: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    isTrue: false,
   },
 ]
 
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {todoList: initialTodosList, userInput: ''}
+
+  onChangeInput = e => {
+    this.setState({userInput: e.target.value})
+  }
 
   deleteTodoList = id => {
     const {todoList} = this.state
@@ -45,18 +59,56 @@ class SimpleTodos extends Component {
     this.setState({todoList: filterTodoList})
   }
 
+  saveTodo = task => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(item =>
+        item.id === task.id ? task : item,
+      ),
+    }))
+  }
+
+  renderTodoInputField = () => {
+    const {userInput} = this.state
+    const onChangeHandler = event => {
+      this.setState({userInput: event.target.value})
+    }
+    const onAddTodo = () => {
+      if (userInput === '') return
+
+      const newList = {id: uuid(), title: userInput}
+      this.setState(prevState => ({
+        todoList: [...prevState.todoList, newList],
+        userInput: '',
+      }))
+    }
+    return (
+      <div className="title-input-container">
+        <input value={userInput} onChange={onChangeHandler} />
+        <button className="add-btn" type="button" onClick={onAddTodo}>
+          Add
+        </button>
+      </div>
+    )
+  }
+
   render() {
-    const {todoList} = this.state
+    const {todoList, count} = this.state
+
     return (
       <div className="todo-container">
+        <h1>{count}</h1>
         <div className="card">
           <h1 className="tido-heading">Simple Todos</h1>
+
+          {this.renderTodoInputField()}
           <ul className="ul-list">
             {todoList.map(eachItem => (
               <TodoItem
                 initialTodosList={eachItem}
                 key={eachItem.id}
                 deleteTodoList={this.deleteTodoList}
+                onChangeInput={this.onChangeInput}
+                saveTodo={this.saveTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,18 +1,57 @@
 // Write your code here
 import './index.css'
+import {useState} from 'react'
+
 const TodoItem = props => {
-  const {initialTodosList, deleteTodoList} = props
+  const {initialTodosList, deleteTodoList, saveTodo} = props
   const {title, id} = initialTodosList
+
+  const [isSaved, setIsSaved] = useState(true)
+  const [newTitle, setNewTitle] = useState(title)
 
   const deleteTodo = () => {
     deleteTodoList(id)
+  }
+
+  const onEditClick = () => setIsSaved(false)
+
+  const onChangeHandler = event => setNewTitle(event.target.value)
+
+  const onSaveTodo = () => {
+    setIsSaved(true)
+    saveTodo({id, title: newTitle})
   }
 
   return (
     <div>
       <div className="item-container">
         <li className="li-list">
-          <p className="item-per">{title}</p>
+          {isSaved ? (
+            <p className="title">{title}</p>
+          ) : (
+            <input
+              className="title"
+              value={newTitle}
+              onChange={onChangeHandler}
+            />
+          )}
+          {isSaved ? (
+            <button
+              type="button"
+              className="delete-btn edit-btn"
+              onClick={onEditClick}
+            >
+              Edit
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="delete-btn save-btn"
+              onClick={onSaveTodo}
+            >
+              Save
+            </button>
+          )}
           <div className="button-card">
             <button className="button" type="button" onClick={deleteTodo}>
               Delete
